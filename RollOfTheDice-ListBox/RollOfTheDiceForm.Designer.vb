b2d1d2a1ee9ab6f286ffbcee82cfbc892ee8b1fb
hoskxavi <hoskmunch@gmail.com>
2022-12-06 17:49:37 -0700

    Add project files.

---
+++ RollOfTheDice-ListBox/RollOfTheDiceForm.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class RollOfTheDiceForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.RollButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.DiceRollListBox = New System.Windows.Forms.ListBox()
+        Me.ListBoxLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'RollButton
+        '
+        Me.RollButton.Location = New System.Drawing.Point(136, 295)
+        Me.RollButton.Name = "RollButton"
+        Me.RollButton.Size = New System.Drawing.Size(156, 67)
+        Me.RollButton.TabIndex = 0
+        Me.RollButton.Text = "Roll the Dice"
+        Me.RollButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(313, 295)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(156, 67)
+        Me.ClearButton.TabIndex = 1
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(494, 295)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(156, 67)
+        Me.ExitButton.TabIndex = 2
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'DiceRollListBox
+        '
+        Me.DiceRollListBox.ColumnWidth = 1
+        Me.DiceRollListBox.FormattingEnabled = True
+        Me.DiceRollListBox.ItemHeight = 20
+        Me.DiceRollListBox.Location = New System.Drawing.Point(136, 63)
+        Me.DiceRollListBox.Name = "DiceRollListBox"
+        Me.DiceRollListBox.Size = New System.Drawing.Size(514, 144)
+        Me.DiceRollListBox.TabIndex = 3
+        '
+        'ListBoxLabel
+        '
+        Me.ListBoxLabel.AutoSize = True
+        Me.ListBoxLabel.Location = New System.Drawing.Point(336, 23)
+        Me.ListBoxLabel.Name = "ListBoxLabel"
+        Me.ListBoxLabel.Size = New System.Drawing.Size(112, 20)
+        Me.ListBoxLabel.TabIndex = 4
+        Me.ListBoxLabel.Text = "Roll of the Dice"
+        '
+        'RollOfTheDiceForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ListBoxLabel)
+        Me.Controls.Add(Me.DiceRollListBox)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.RollButton)
+        Me.Name = "RollOfTheDiceForm"
+        Me.Text = "Roll of the Dice"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents RollButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents DiceRollListBox As ListBox
+    Friend WithEvents ListBoxLabel As Label
+End Class
---
+++ RollOfTheDice-ListBox/RollOfTheDiceForm.vb
@@ -0,0 +1,82 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class RollOfTheDiceForm
+
+    'Sub will create the header when the form loads
+    Private Sub RollOfTheDiceForm_Load() Handles Me.Load
+        Dim rows As Integer = 1
+        Dim columns As Integer = 11
+        Dim header(columns + 1) As Integer
+        Dim columnWidth As Integer = 9
+        Dim columnSeparator As String = " |"
+        Dim horizontalSeparator As String = "-"
+
+        For i = 2 To UBound(header)
+            header(i) = i
+        Next
+
+        DiceRollListBox.Items.Add(FormatArray(header, columnWidth, columnSeparator))
+        DiceRollListBox.Items.Add(StrDup(76, horizontalSeparator))
+    End Sub
+
+    'Roll button will roll the dice 1000 times and list the numbers rolled
+    Private Sub RollButton_Click(sender As Object, e As EventArgs) Handles RollButton.Click
+        Dim numberOfColumns As Integer = 11
+        Dim rolls(numberOfColumns + 1) As Integer
+        Dim header(numberOfColumns + 1) As Integer
+        Dim columnWidth As Integer = 8
+        Dim columnSeparator As String = " |"
+
+
+        'roll the dice 1000 times
+        For i = 0 To 1000
+            rolls(DiceRoll()) += 1
+        Next
+
+        DiceRollListBox.Items.Add(FormatArray(rolls, columnWidth, columnSeparator))
+
+    End Sub
+
+    'Clear button will clear the rolled dice numbers
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        DiceRollListBox.Items.Clear()
+        RollOfTheDiceForm_Load()
+    End Sub
+
+    'Exit button simply exits the program
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        MsgBox("Have a just ok day.")
+        Me.Close()
+    End Sub
+
+
+
+    'Random number generator to roll the dice and produce a value
+    Function DiceRoll() As Integer
+        Dim firstDie As Integer
+        Dim secondDie As Integer
+        Dim total As Integer
+
+        Randomize()
+
+        firstDie = CInt(Int(6 * Rnd() + 1))
+        secondDie = CInt(Int(6 * Rnd() + 1))
+        total = firstDie + secondDie
+
+        Return total
+    End Function
+
+
+    'Function to format the array of dice rolls
+    Function FormatArray(columns() As Integer, columnWidth As Integer, columnSeparator As String) As String
+        Dim _row As String
+        Dim temp As String
+        For i = (LBound(columns) + 2) To UBound(columns)
+            temp = columns(i) & columnSeparator
+            temp = temp.PadLeft(columnWidth)
+            _row &= temp
+        Next
+        Return _row
+    End Function
+
+End Class
